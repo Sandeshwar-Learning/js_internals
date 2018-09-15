f86446e07a156310b2b23901663a9752ffa9e736
Sandeshwar Sharma <ssandeshwar@gmail.com>
2018-09-15 12:53:41 +0530

    dynamic data; data types

---
+++ first.js
@@ -0,0 +1,23 @@
+g_a = 5
+g_b = 0
+g_c = 7
+
+function add(a, b) {
+    var c
+    var x
+    c = a + b
+    x += c
+
+    return c
+}
+
+var x = 5
+var y = 7
+var z
+
+z = add(x, y)
+
+console.log(z);
+
+console.log(add.toString());
+
---
+++ var.js
@@ -0,0 +1,47 @@
+/*
+ * Filename: e:\Workspace\javascript\internals\var.js
+ * Path: e:\Workspace\javascript\internals
+ * Created Date: Saturday, September 15th 2018, 12:24:50 pm
+ * Author: devsa
+ * 
+ * Copyright (c) 2018 Your Company
+ */
+
+ var x = 5
+ console.log(x);
+ 
+ x = "hey ya"
+ console.log(x);
+
+ x = true
+ console.log(x);
+ 
+ x = function() {
+     console.log("I'm x");     
+ }
+ console.log(x);
+
+ // add 2 numbers
+ console.log(2 + 4);
+ 
+ // two strings
+ console.log("hey" + "ya");
+ 
+ // string + number
+ console.log("High" + 5);
+ 
+ // two number + string
+ console.log(5 + 5 + "five");
+
+ // string + two numbers
+ console.log("chill" + 5 + 5);
+ 
+ // number + boolean
+ console.log(5 + true);
+ 
+ // boolean + string
+ console.log(false + "wrong");
+ 
+ // boolean + number
+ console.log(false + 9);
+ 
