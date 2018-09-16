805efbc80bd71cd149d3d27c311f8c0ecbc8cdb4
Sandeshwar Sharma <dev.sandeshwar@outlook.com>
2018-09-16 14:38:52 +0530

    extend class

--- Employee using class keyword.js
+++ Employee using class keyword.js
@@ -10,6 +10,20 @@ class Employee {
     }
 }
 
-var riya = new Employee(1, "Riya Sharma", 12000);
-let abhishek = new Employee(2, "Abhishek Jain", 17000);
+// var riya = new Employee(1, "Riya Sharma", 12000);
+// let abhishek = new Employee(2, "Abhishek Jain", 17000);
 
+class Manager extends Employee {
+    constructor(id, name, salary, perks) {
+        super(id, name, salary);
+        this.perks = perks;
+    }
+
+    print() {
+        super.print()
+        console.log("Perks: ", this.perks);
+    }
+}
+
+let kartik = new Manager(3, "Kartik Deshpande", 30000, ["Car", "Stay"]);
+kartik.print();
