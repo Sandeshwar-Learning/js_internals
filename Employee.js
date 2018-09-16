b0bbd2e544f48ad2d3167b1f43bd231bf076d8f9
Sandeshwar Sharma <dev.sandeshwar@outlook.com>
2018-09-16 12:33:48 +0530

    instance and static members; prototypes

--- Employee.js
+++ Employee.js
@@ -1,10 +1,23 @@
 function Employee(id, name, salary) {
+    // local to function
+    let temp = "This variable will get destroyed as soon as the function is finished executing";
+
+    // instance members (will be available in the instance)
     this.id = id;
     this.name = name;
     this.salary = salary;
     this.print = () => {
         console.log(`(${this.id}) Name: ${this.name}, Salary: ${this.salary}`)
     }
+    
+    // static member (will be stored in the function object)
+    if(!Employee.totalEmployees) {
+        Employee.totalEmployees = 0;
+    }
+    Employee.totalEmployees++;    
+
+    // base member (will be stored in the functions prototype object)
+    Employee.prototype.baseHra = 5000;
 }
 
 let vinod = new Employee(1, "Vinod Gounder", 21000);
@@ -12,3 +25,5 @@ let rakesh = new Employee(2, "Rakesh Gomes", 18000);
 
 vinod.print();
 rakesh.print();
+
+//console.log("Total Employees: ", Employee.totalEmployees);
